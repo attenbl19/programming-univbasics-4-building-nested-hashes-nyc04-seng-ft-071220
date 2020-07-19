5154579207df83dad8086256d341530c822f9be9
Lida Attenborough <attenboroughl@protonmail.com>
2020-07-19 02:41:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,10 +37,8 @@ def monopoly_with_third_tier
       :reading_railroad => {},
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
-      
-    
-    
-  }
+      :shortline_railroad => {}
+    }
 
 end
 
