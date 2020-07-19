a6c0bf79395996eeb3a23b057c7164315b1032a1
Lida Attenborough <attenboroughl@protonmail.com>
2020-07-19 02:49:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,10 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {
-    :railroads
+    :railroads => {
+      
+      
+    
     :rent_in_dollars => {
       :one_piece_owned => 25,
       :two_pieces_owned => 50,
